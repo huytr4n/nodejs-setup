0bf07bcf1a00f8b3b2fb7808ee042bbbfde62506
Huy Tran <huy.tranquoc@asnet.com.vn>
2015-11-12 16:06:05 +0700

    Move log out of bash scope

--- recipes/npm_package.rb
+++ recipes/npm_package.rb
@@ -1,12 +1,13 @@
 log 'install npm packages'
 
 # install npm packages
-if node['nodejs-setup']['npm_packages']
+if node['nodejs-setup'].key?('npm_packages')
   packages = node['nodejs-setup']['npm_packages']
 
   packages.each do |name|
+    log 'Install node package #{name}'
+
     bash 'setup npm package' do
-      log 'Install node package #{name}'
       code <<-EOH
         sudo npm install -g #{name}
         EOH
